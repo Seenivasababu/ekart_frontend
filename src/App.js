967b8fc451a08c1cc3206d6b77b6b4648653124c
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-08-10 22:43:03 +0530

    Update App.js
    Defined the requirements very clearly

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>We are going to create E Kart app</h1>
+      <h1>We are going to create basic Functionality of fetching product from Database and sorting the product and some searching and filtering functionality</h1>
     </div>
   );
 }
